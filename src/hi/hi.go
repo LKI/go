2451a74fa940ca8b151ee4c3051a2343be008a82
Lirian Su <liriansu@gmail.com>
2017-07-22 20:42:01 +0800

    feat(hi): ok, tabs. try defer and error

--- src/hi/hi.go
+++ src/hi/hi.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-    fmt.Println("Hi, Lirian")
+	fmt.Println("Hi, Lirian")
+
+	defer tryDefer()
+
+	fmt.Println(tryError(true))
+	fmt.Println(tryError(false))
+
+}
+
+func tryDefer() {
+	fmt.Println("this is a defer")
+}
+
+func tryError(expectError bool) error {
+	if expectError {
+		return errors.New("new error")
+	} else {
+		return nil
+	}
 }
